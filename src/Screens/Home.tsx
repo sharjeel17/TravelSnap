e8a21a27103ad05af72e04f6d8c1c2bd1298abb7
sharjeel <sharjeel-nawaz00@hotmail.com>
2023-12-11 01:12:11 +0000

    Basic navigation done for testing

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,19 +1,26 @@
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { useEffect, useState } from 'react';
-import { Button, FlatList, Image, ListRenderItem, Pressable, ScrollView, Text, View } from "react-native";
+import { Button, FlatList, GestureResponderEvent, Image, ListRenderItem, Pressable, ScrollView, Text, View } from "react-native";
 
 interface Photos {
+    albumId: number,
+    id: number,
+    title: string,
+    url: string,
     thumbnailUrl: string
 }
 
 export default function Home({navigation}: NativeStackScreenProps<any>){
     const [photos, setPhotos] = useState([]);
+    //get posts from url
     const getPosts = async () => {
         try{
         const response = await fetch("https://jsonplaceholder.typicode.com/photos?_limit=10");
         const data = await response.json();
         setPhotos(data);
-        } catch (err){
+
+        } 
+        catch (err){
             console.error(err);
         }
     }
@@ -22,21 +29,26 @@ export default function Home({navigation}: NativeStackScreenProps<any>){
         getPosts();
     }, [])
 
+    function goToDetailsPage(item: Photos){
+        navigation.navigate("Details", item);
+    }
+
   return (
     <View>
-            <FlatList 
-             data={photos}
-             renderItem= {({item}: {item: Photos}) => {
-                console.log(item.thumbnailUrl);
-                return (
-                    <Pressable>
-                        <Image source={{uri: item.thumbnailUrl+"png"}} style={{width: 400, height: 400}} />
-                        <Text>Should render here</Text>
-                    </Pressable>
-                )
-             }} 
+        <FlatList 
+            data={photos}
+            renderItem = {({item}: {item: Photos}) => {
+            return (
+                <Pressable onPress={() => {
+                    goToDetailsPage(item)
+                }}>
+                    <Image source={{uri: item.thumbnailUrl+"png"}} style={{width: 400, height: 400}} />
+                    <Text>Should render here</Text>
+                </Pressable>
+            )
+            }} 
 
-            />
+        />
     </View>
   );
 }
--- src/Screens/PhotoDetails.tsx
+++ src/Screens/PhotoDetails.tsx
@@ -1,9 +1,13 @@
-import { Text, View } from "react-native";
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Image, Text, View } from "react-native";
+import { Photos } from '../types/types';
 
-export default function PhotoDetails(){
+export default function PhotoDetails({route}: NativeStackScreenProps<any>){
+    const item = route.params;
     return (
-        <View>
-            <Text>This is the details page</Text>
+        <View className="items-center">
+            <Image className="w-60 h-60" source={{uri: item?.thumbnailUrl + ".png"}} />
+            <Text>{item?.title}</Text>
         </View>
     );
 }
