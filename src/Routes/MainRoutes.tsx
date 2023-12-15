2bbe04c4eb900231d62a05348e00accf2fac6e81
sharjeel <sharjeel-nawaz00@hotmail.com>
2023-12-15 05:33:56 +0000

    added tab icons

--- src/Routes/MainRoutes.tsx
+++ src/Routes/MainRoutes.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import UploadPhoto from "../pages/Upload";
 import Profile from "../pages/Profile";
 import HomeRoute from "./HomeRoute";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,9 +12,43 @@ export default function MainRoutes(){
   return (
     //HomeRoute uses stack navigator
       <Tab.Navigator initialRouteName="Home Screen">
-          <Tab.Screen name="Home Page" component={HomeRoute} options={{headerShown: false}}/>
-          <Tab.Screen name="Upload Photo" component={UploadPhoto} options={{headerTitle:"Upload Photo"}}/>
-          <Tab.Screen name="Profile" component={Profile} options={{headerTitle: "Profile"}}/>
+          <Tab.Screen name="Home Page" component={HomeRoute} 
+          options={{
+            headerShown: false, 
+            tabBarIcon: ({ focused }) => (
+              <Ionicons 
+                style={{
+                  opacity: focused ? 0.95 : 0.5,
+                  marginBottom: focused ? 6 : 0,
+                  fontSize: 26
+                }}
+                name="home-outline"
+              />
+            ),
+          }}
+          />
+          <Tab.Screen name="Upload Photo" component={UploadPhoto} options={{headerTitle:"Upload Photo", 
+          tabBarIcon: ({ focused }) => (
+              <Ionicons 
+                style={{
+                  opacity: focused ? 0.95 : 0.5,
+                  marginBottom: focused ? 6 : 0,
+                  fontSize: 26
+                }}
+                name="camera-outline"
+              />
+            )}} 
+            />
+          <Tab.Screen name="Profile" component={Profile} options={{headerTitle: "Profile", tabBarIcon: ({ focused }) => (
+              <Ionicons 
+                style={{
+                  opacity: focused ? 0.95 : 0.5,
+                  marginBottom: focused ? 6 : 0,
+                  fontSize: 26
+                }}
+                name="person-outline"
+              />
+            )}}/>
       </Tab.Navigator>
   );
 };
--- src/pages/PhotoDetails.tsx
+++ src/pages/PhotoDetails.tsx
@@ -1,5 +1,5 @@
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Image,Keyboard,KeyboardAvoidingView, Platform, Text, TouchableWithoutFeedback, View } from "react-native";
+import { Image,Keyboard,KeyboardAvoidingView,Platform,ScrollView,Text, TouchableWithoutFeedback, View } from "react-native";
 import { Photos } from '../types/types';
 import Comments from '../components/Comments';
 import { useHeaderHeight } from '@react-navigation/elements'
@@ -12,13 +12,13 @@ export default function PhotoDetails({route}: NativeStackScreenProps<any>){
     return (
         
         <View className='flex-1 bg-[#1A1C20]'>
-            <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding": "height"} keyboardVerticalOffset={height + 100} className='flex-1 bg-[#1A1C20]'>
+            <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} keyboardVerticalOffset={height + 400} className='flex-1 bg-[#1A1C20]'>
                 <View>
-                    <TouchableWithoutFeedback className="flex-1 bg-white" onPress={Keyboard.dismiss}>
+                    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
                         <View>
                             <Text className="mb-1 mt-2 pl-3 text-[#FAFAFA] text-sm shadow-lg">@{photo.PostedBy}</Text>
-                            <Image className="h-[200] w-[250] self-center" source={{uri: photo.Photo}} />
-                            <Text className="mt-2 mb-2 pl-3 text-[#FAFAFA] text-sm shadow-lg">{photo.Caption}</Text>
+                            <Image className="w-full h-3/6 self-center" source={{uri: photo.Photo}} />
+                            <Text className="mt-2 mb-5 pl-3 text-[#FAFAFA] text-sm shadow-lg">{photo.Caption}</Text>
                         </View>
                     </TouchableWithoutFeedback>
                     <Comments photoId={photo.Id} />
